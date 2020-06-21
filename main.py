5bc379948283d4371fb5ec99ca7f4443c977a903
彭涛 <pengtaopt01@163.com>
2020-06-21 12:16:23 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+import os
+import requests
+import qrcode
+import datetime
+import configparser
+
+# load confg
+conf= configparser.ConfigParser()
+conf.read('./config.conf')
+
+userCode = conf.get('basic', 'username')
+userPass = conf.get('basic', 'password')
+wechatGroupNumber = input('Input WeChat Group Count: ')
+setSuccess = 'http://sm.ewmtool.com/insertRecord?codeSuccess=0&recordId='
+setError = 'http://sm.ewmtool.com/insertRecord?codeSuccess=1&recordId='
+# now date time
+nowDate = datetime.datetime.now().strftime("%Y-%m-%d")
+nowTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%i:%s")
+
+
+# Login POST
+getLogin = requests.post('http://sm.ewmtool.com/getLogin', data={'userCode': userCode, 'userPass': userPass})
+
+print(getLogin.json())
+
+# put wechat group number
+userDayCount = requests.post('http://sm.ewmtool.com/userDayCount', data={'number': wechatGroupNumber}, cookies=getLogin.cookies)
+
+print(userDayCount.json())
+
+savePath = './qrcode/' + nowDate + '/'
+
+if not os.path.exists(savePath):
+    os.makedirs(savePath)
+
+dataFile = open('./data/data-'+nowDate+'.json', 'a')
+gone = True
+while(gone):
+# get qr code
+    getQr = requests.post('http://sm.ewmtool.com/getQr', cookies=getLogin.cookies)
+
+    jsonData = getQr.json()
+    if jsonData['code'] != 200:
+        print(jsonData)
+        gone=False
+        break
+    print(jsonData)
+    dataFile.write(nowTime+'===='+getQr.text+"\n")
+    # make code 
+    img = qrcode.make(jsonData['data']['imgsrc'])
+    img.save(savePath + jsonData['data']['name'] + '.png')
+
+    print("------------------------------------------------------------")
+
+
